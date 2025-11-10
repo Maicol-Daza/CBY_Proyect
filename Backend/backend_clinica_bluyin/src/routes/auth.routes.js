4a7a4ca488bc45506ff5551b5c3c39d90f3c03d3
SamNube <samuel.riosaenz.8.1@gmail.com>
2025-11-10 16:57:39 -0500

    Delete Backend directory
    prueba

--- Backend/backend_clinica_bluyin/src/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../controllers/auth.controller');
-
-router.post('/login', (req, res) => AuthController.login(req, res));
-
-module.exports = router;
